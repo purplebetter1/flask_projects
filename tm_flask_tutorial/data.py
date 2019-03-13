849e525321a5b22ce67c1501cfa79d43376c9a56
Andrew <booleanorama@gmail.com>
2019-03-13 20:25:10 +0000

    first commit

---
+++ tm_flask_tutorial/data.py
@@ -0,0 +1,21 @@
+def Articles():
+    articles = [
+        {'id': 1,
+         'title':'Article One',
+         'body': 'Some text goes here',
+         'author':'Clark Kent',
+         'created_at':'03-13-19'
+        },
+        {'id': 2,
+         'title': 'Article Two',
+         'body':'Some text goes here',
+         'author': 'Joe Schmoe',
+         'created_at': '02-27-19'
+        },
+        {'id':3,
+         'title': 'Article Three',
+         'body': 'Some text goes here',
+         'author': 'Peter Parker',
+         'created_at': '12-26-17'
+        }]
+    return articles
